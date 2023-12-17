63d15b9bc7a065c434a24a5ef25619fbe5355135
Artem Minich <temic543.9@gmail.com>
2023-12-17 13:38:02 +0200

    add Dockerfile

--- src/main/java/com/cooldelivery/delivery/config/SecurityConfig.java
+++ src/main/java/com/cooldelivery/delivery/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                 )
                 .logout((logout) -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
+                        .logoutSuccessUrl("/home")
                         .permitAll()
                 );
 
